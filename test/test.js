d0320af8f173563223987529d4c23fdc1bfb1004
Lux Pandora <luxizzle@gmail.com>
2017-02-12 17:01:05 +0100

    eslint is cool. pretty code is nice.

--- test/test.js
+++ test/test.js
@@ -1,25 +1,25 @@
-var Discordful = require('../index.js')
+var Discordful = require('../index.js');
 var client = new Discordful({
     autoReconnect: true
-})
+});
 
-var CommandParser = require('./parser.js')
-var parser = new CommandParser(client.discordie.User.id, {})
+var CommandParser = require('./parser.js');
+var parser = new CommandParser(client.discordie.User.id, {});
 
-var token = require('./auth.js')
+var token = require('./auth.js');
 
-client.connect({token: token})
+client.connect({token: token});
 
 parser
     .command('ping', {
         description: 'We all know what this does'
     })
     .callback(function(args) {
-        this.reply('pong! ' + args.join(', '))
-    })
+        this.reply('pong! ' + args.join(', '));
+    });
 
 client.event('MESSAGE_CREATE')
     .use(function(e) {
-        return e.message
-    })  
-    .use(parser.parse())
+        return e.message;
+    })
+    .use(parser.parse());
