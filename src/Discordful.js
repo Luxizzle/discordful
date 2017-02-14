dbd9296785fc6e3226485447aea96dc79ace432c
Lux Pandora <luxizzle@gmail.com>
2017-02-14 11:53:14 +0100

    Add base properties

--- src/Discordful.js
+++ src/Discordful.js
@@ -14,6 +14,17 @@ class Discordful {
         var _this = this;
         this.discordie = new Discordie(options);
 
+        this.User = this.discordie.User;
+        this.Users = this.discordie.Users;
+        this.Guilds = this.discordie.Guilds;
+        this.Channels = this.discordie.Channels;
+        this.DirectMessageChannels = this.discordie.DirectMessageChannels;
+        this.Messages = this.discordie.Messages;
+        this.VoiceConnections = this.discordie.VoiceConnections;
+        this.Webhooks = this.discordie.Webhooks;
+        this.UnavailableGuilds = this.discordie.UnavailableGuilds;
+        this.autoReconnect = this.discordie.autoReconnect;
+
         this.event('GATEWAY_READY')
             .use(function() {
                 log('Connected as ' + _this.discordie.User.username + '#' + _this.discordie.User.discriminator);
@@ -23,6 +34,7 @@ class Discordful {
 
         return this;
     }
+
     /*
      * @function
      * @param {Object} options - Options to connect with such as `token`
@@ -30,6 +42,11 @@ class Discordful {
      */
     connect(options) {
         this.discordie.connect(options);
+
+        return this;
+    }
+    disconnect() {
+        this.discordie.disconnect();
 
         return this;
     }
@@ -41,6 +58,9 @@ class Discordful {
     event(event) {
         return new EventPipeline(this, event);
     }
+
+    get state() { return this.discordie.state; }
+    get connected() { return this.discordie.connected; }
 }
 
 module.exports = Discordful;
